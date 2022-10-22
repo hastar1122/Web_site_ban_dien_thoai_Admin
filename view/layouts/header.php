a1340e8280090a5c2d8948625a55af9708d0eded
NguyenHoangAnh <hoanganh08102000@gmail.com>
2022-10-22 15:02:00 +0700

    Sửa UI Thanh side bar

--- view/layouts/header.php
+++ view/layouts/header.php
@@ -50,29 +50,30 @@
             <li class="nav-item active">
                 <a class="nav-link" href="../index.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
+                    <span>Tổng quan</span></a>
             </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider">
 
             <!-- Heading -->
-            <div class="sidebar-heading">
+            <!-- <div class="sidebar-heading">
                 Interface
-            </div>
+            </div> -->
 
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+                    <span>Quản lý danh mục</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="../pages/buttons.php">Buttons</a>
-                        <a class="collapse-item" href="../pages/cards.php">Cards</a>
+                        <!-- <h6 class="collapse-header">Custom Components:</h6> -->
+                        <a class="collapse-item" href="../pages/buttons.php">Thương hiệu</a>
+                        <a class="collapse-item" href="../pages/cards.php">Loại sản phẩm</a>
+                        <a class="collapse-item" href="../pages/cards.php">Khách hàng</a>
                     </div>
                 </div>
             </li>
@@ -82,15 +83,15 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
+                    <span>Quản lý sản phẩm</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="../pages/utilities-color.php">Colors</a>
-                        <a class="collapse-item" href="../pages/utilities-border.php">Borders</a>
-                        <a class="collapse-item" href="../pages/utilities-animation.php">Animations</a>
+                        <!-- <h6 class="collapse-header">Custom Utilities:</h6> -->
+                        <a class="collapse-item" href="../pages/utilities-color.php">Danh sách sản phẩm</a>
+                        <a class="collapse-item" href="../pages/utilities-border.php">Thêm mới sản phẩm</a>
+                        <a class="collapse-item" href="../pages/utilities-animation.php">Nhập kho</a>
                         <a class="collapse-item" href="../pages/utilities-other.php">Other</a>
                     </div>
                 </div>
@@ -100,12 +101,12 @@
             <hr class="sidebar-divider">
 
             <!-- Heading -->
-            <div class="sidebar-heading">
+            <!-- <div class="sidebar-heading">
                 Addons
-            </div>
+            </div> -->
 
             <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
+            <!-- <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
                     aria-expanded="true" aria-controls="collapsePages">
                     <i class="fas fa-fw fa-folder"></i>
@@ -123,20 +124,20 @@
                         <a class="collapse-item" href="pages/blank.php">Blank Page</a>
                     </div>
                 </div>
-            </li>
+            </li> -->
 
             <!-- Nav Item - Charts -->
             <li class="nav-item">
                 <a class="nav-link" href="pages/charts.php">
                     <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
+                    <span>Đơn hàng</span></a>
             </li>
 
             <!-- Nav Item - Tables -->
             <li class="nav-item">
                 <a class="nav-link" href="pages/tables.php">
                     <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
+                    <span>Báo cáo doanh thu</span></a>
             </li>
 
             <!-- Divider -->
@@ -148,11 +149,11 @@
             </div>
 
             <!-- Sidebar Message -->
-            <div class="sidebar-card d-none d-lg-flex">
+            <!-- <div class="sidebar-card d-none d-lg-flex">
                 <img class="sidebar-card-illustration mb-2" src="../../img/undraw_rocket.svg" alt="...">
                 <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
                 <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
-            </div>
+            </div> -->
 
         </ul>
         <!-- End of Sidebar -->
